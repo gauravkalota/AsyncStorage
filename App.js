8ec8627a879ee97a9f61ba459281903190195663
gaurav <gaurav@acmeminds.com>
2021-08-11 08:28:04 +0530

    firstcommit

--- App.js
+++ App.js
@@ -1,14 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, AsyncStorage } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component  {
+  state= {
+    item: 'loading'
+  }
+  async  componentDidMount() {                           // value stay kare fetch ke baad if we reload the stimulator
+     this.setState({
+       item: await AsyncStorage.getItem('mykey')
+     })
+
+   }
+
+
+  storeData = async () => {
+    try {
+      await AsyncStorage.setItem('mykey', 'AsynxStorage -- Is Activated');
+      this.setState({
+        item: await AsyncStorage.getItem('mykey')
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  deleteData = async () => {
+    try {
+      AsyncStorage.removeItem('mykey', async () => {
+        console.log("delete")
+        this.setState({
+          item: await AsyncStorage.getItem('mykey')
+        })
+      });
+    } catch (error) {
+
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{fontSize:40, fontWeight:'bold'}}>ASYNCSTORAGE</Text>
+        <Button
+          title="Store"
+          onPress={this.storeData}
+        />
+        <Button
+          title="Delete"
+          onPress={this.deleteData}
+        />
+        <Text style={{ fontSize: 30 }}>{this.state.item}</Text>
+      </View>
+    );
+  }
+
+
 }
 
 const styles = StyleSheet.create({
